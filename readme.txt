5abafe2191b0cab3b3d23cd9a9af2b645a42894a
sanathreddy444 <sanathreddy444@gmail.com>
2021-04-06 13:33:44 -0400

    updadted script file

---
+++ readme.txt
@@ -0,0 +1,12 @@
+Car Rental Project
+
+Referred Links:
+https://medium.com/@rahulguptalive/create-crud-apis-in-nodejs-express-and-mysql-abda4dfc2d6
+
+
+
+Tools:
+nodejs version: v12.16.1
+mysql: MySQL Community Server - GPL 8.0.23 with workbench
+
+
--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const dbConn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'Reddy@1510',
-  database : 'car_rental',
-  // port     : 3306
+  database : 'car_rental'
 });
 dbConn.connect(function(err) {
   if (err) throw err;
@@ -51,11 +50,24 @@ var Customer = function(customer){
   this.created_date   = new Date();
 };
 
+var Inventory = function(inventory){
+  this.model     = inventory.model;
+  this.brand      = inventory.brand;
+  this.year          = inventory.year;
+  this.vehicle_type   = inventory.vehicle_type;
+  this.is_active      = 1;
+  this.price       = inventory.price;
+  this.booked_by            = null;
+  this.created_date   = new Date();
+};
 
 // define a root route
 // app.get('/', (req, res) => {
 //   res.send("Hello World");
 // });
+
+// ALTER TABLE car_rental.vehicle ADD booked_by varchar(200);
+// ALTER TABLE car_rental.customer MODIFY COLUMN dob varchar(100)
 
 app.post('/register', (req, res) => {
   // console.log("Request", req.body)
@@ -87,6 +99,145 @@ app.post('/login', (req, res) => {
     }
   });
 });
+
+app.get('/fetchAllCustomers', (req,res) => {
+  dbConn.query("SELECT * FROM customer", function(err, result) {
+    if(err) {
+      res.send({status:500, message: "Please try again"});
+    } else {
+      res.send({status:200,data:result})
+    }
+  })
+})
+
+app.post('/addInventory', (req, res) => {
+  const new_inventory = new Inventory(req.body);
+
+  dbConn.query("INSERT INTO vehicle set ?", new_inventory, function (err, result) {
+    if(err) {
+      console.log
+      res.send({status:500, message: "Please try again"});
+    }
+    else{
+      res.send({status:200, message: "Vehicle added successfully"});
+    }
+  });
+});
+
+app.post('/fecthAvailableinventory', (req, res) => {
+  let query = "SELECT * FROM vehicle WHERE is_active = ?";
+  let values = [1]
+  if(req.body.vehicle_type!="All") {
+    query+=" and vehicle_type = ?";
+    values.push(req.body.vehicle_type);
+  }
+  if(req.body.year!="All") {
+    query+=" and year = ?";
+    values.push(req.body.year);
+  }
+  if(req.body.brand!="All") {
+    query+=" and brand = ?";
+    values.push(req.body.brand);
+  }
+  dbConn.query(query,values,function(err, result) {
+    if(err) {
+      res.send({status:500, message: "Please try again"});
+    }
+    else{
+      res.send({status:200, data: result});
+    }
+  })
+})
+
+app.get('/deleteInventory/:id', (req, res) => {
+  dbConn.query("DELETE FROM vehicle WHERE id = ?",[req.params.id], function(err, result) {
+    if(err) {
+      console.log("err",err);
+      res.send({status:500, message: "Please try again"});
+    }
+    else{
+      res.send({status:200, message: "Car Deleted Successfully"});
+    }
+  })
+})
+
+app.get('/customerProfile/:username', (req, res) => {
+  dbConn.query("SELECT * FROM customer WHERE email = ?",[req.params.username], function(err, result) {
+    if(err) {
+      res.send({status:500, message: "Please try again"});
+    } else if(result.length) {
+      res.send({status: 200, data:result[0]});
+    } else {
+      res.send({status:500, message: "User Not Found"});
+    }
+  })
+});
+
+app.post('/updateProfile', (req, res) => {
+  dbConn.query("UPDATE customer SET first_name = ?,last_name = ?,dob = ?,address = ?,phone_number = ?,driving_license = ?",[req.body.first_name,req.body.last_name,req.body.dob,req.body.address,req.body.phone_number,req.body.driving_license], function(err, result) {
+    if(err) {
+      res.send({status:500, message: "Please try again"});
+    } else {
+      res.send({status: 200, message: "Profile Updated Successfully"});
+    }
+  })
+})
+
+app.get('/confirmBooking/:id/:username', (req, res) => {
+  dbConn.query("UPDATE vehicle SET is_active = ?,booked_by = ? WHERE id = ?",[2,req.params.username,req.params.id], function(err, result) {
+    if(err) {
+      console.log("err",err);
+      res.send({status:500, message: "Please try again"});
+    } else {
+      res.send({status: 200, message: "Vehicle Booked Successfully"});
+    }
+  })
+})
+
+app.post('/fetchBookedCars', (req, res) => {
+  dbConn.query("SELECT * FROM vehicle WHERE booked_by = ?",[req.body.username], function(err, result) {
+    if(err) {
+      console.log("err",err);
+      res.send({status:500, message: "Please try again"});
+    } else {
+      res.send({status: 200, data: result});
+    }
+  })
+})
+
+app.get('/returnCar/:id', (req, res) => {
+  dbConn.query("UPDATE vehicle SET is_active =?,booked_by = ? WHERE id = ?",[3,null,req.params.id], function(err, result) {
+    if(err) {
+      console.log("err",err);
+      res.send({status:500, message: "Please try again"});
+    } else {
+      res.send({status: 200, message: "Vehicle Returned Successfully"});
+    }
+  })
+})
+
+app.get('/fetchReturnedCars', (req, res) => {
+  dbConn.query("SELECT * FROM vehicle WHERE is_active = ?",[3], function(err, result) {
+    if(err) {
+      console.log("err",err);
+      res.send({status:500, message: "Please try again"});
+    } else {
+      res.send({status: 200, data: result});
+    }
+  })
+})
+
+app.get('/confirmReturn/:id', (req, res) => {
+  dbConn.query("UPDATE vehicle SET is_active =? WHERE id = ?",[1,req.params.id], function(err, result) {
+    if(err) {
+      console.log("err",err);
+      res.send({status:500, message: "Please try again"});
+    } else {
+      res.send({status: 200, message: "Vehicle Returned Successfully"});
+    }
+  })
+})
+
 // listen for requests
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000`);
